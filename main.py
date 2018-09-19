ea26b5abaf9f467e1fd87ddf91cec53d94b75f28
Renan Verissimo de vasconcelos <renanverissimo@uol.com.br>
2018-09-19 10:50:47 -0300

    Adicionado exibição agrupamentos + porcentagem de acerto

--- main.py
+++ main.py
@@ -21,7 +21,29 @@ def criacaoCentroideRandom():
     return array
 
 def avaliacaoAcertos(arrayAnalise):
-    return
+    g1 = 0
+    g2 = 0
+    g3 = 0
+    acertos = 0
+
+    for i in range(0, len(arrayAnalise)):
+        if (arrayAnalise[i] == 0):
+            g1+=1
+
+        if (arrayAnalise[i] == 1):
+            g2+=1
+
+        if (arrayAnalise[i] == 2):
+            g3+=1
+
+        if (i == 49) or (i == 99) or (i == 149):
+            print("Agrupamento:", g1, g2, g3)
+            acertos += max(g1, g2, g3) - (min(g1, g2) + min(g2, g3))
+            g1 = 0
+            g2 = 0
+            g3 = 0
+
+    return round(acertos/150*100, 2)
 
 for i in range(1, 4):
     if (i != 3):
@@ -37,6 +59,7 @@ for i in range(1, 4):
     plt.xlabel('Largura da Petala')
     plt.ylabel('Comprimento da Sepala')
     plt.legend()
-    print(avaliacaoAcertos(trabmeans.labels_))
+    print("Porcentagem acerto - Grupo " + str(i) + ":", avaliacaoAcertos(trabmeans.labels_))
+    print("\n")
 
-#plt.show()
+plt.show()
